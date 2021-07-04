ee79141666821194edf54f11ffda8a468dc7cf78
Diego Obando <diegoobando20@gmail.com>
2021-07-04 18:01:58 -0400

    parsear modelo de datos

---
+++ app/src/main/java/com/dieg0code/doglist/APIService.kt
@@ -0,0 +1,10 @@
+package com.dieg0code.doglist
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+    @GET
+    fun getDogsByBreeds(@Url url: String): Response<DogsResponse>
+}
