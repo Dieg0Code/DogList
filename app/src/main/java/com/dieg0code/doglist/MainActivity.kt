5e2de82cc7ada59f8f21ac5d7ff6e7abef8c8309
Diego Obando <diegoobando20@gmail.com>
2021-07-04 16:24:18 -0400

    initial proyect setup (picasso, retrofit2, internet access and viewBinding)

---
+++ app/src/main/java/com/dieg0code/doglist/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dieg0code.doglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dieg0code.doglist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
