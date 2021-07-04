e0f08d079bbdec9fb2a4d007c6765a574701416f
Diego Obando <diegoobando20@gmail.com>
2021-07-04 18:23:49 -0400

    creando retrofit

--- app/src/main/java/com/dieg0code/doglist/MainActivity.kt
+++ app/src/main/java/com/dieg0code/doglist/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.dieg0code.doglist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dieg0code.doglist.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +18,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    // retrofit basic config
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful) {
+                // show recyclerView
+            } else {
+                // show error
+            }
+        }
     }
 }
